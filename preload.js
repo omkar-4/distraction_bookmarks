270c09f1e011851e7eef97929416ea8ef16d9a1c
omkar-4 <omkar.pro04@gmail.com>
2024-09-13 10:02:06 +0530

    reset

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-// Expose update-related APIs to the frontend
-contextBridge.exposeInMainWorld("electronAPI", {
-  onUpdateAvailable: (callback) => ipcRenderer.on("update_available", callback),
-  onUpdateDownloaded: (callback) =>
-    ipcRenderer.on("update_downloaded", callback),
-  restartApp: () => ipcRenderer.send("restart_app"),
-});
